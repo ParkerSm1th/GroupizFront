5d81b14f8a74ab4f10c1a156ac92c0f533c6d3f4
Parker Smith <parker@yeetdev.com>
2021-02-13 16:42:22 -0800

    Basic quiz code

--- routes/index.js
+++ routes/index.js
@@ -6,15 +6,12 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
-router.get('/quiz/:quizCode', (req, res) => {
+router.get('/:quizCode', (req, res) => {
   console.log('Request for quiz page recieved: ' + req.params.quizCode);
   res.render('quiz', {quizCode: req.params.quizCode});
 });
 
-router.get('/contact', (req, res) => {
-  console.log('Request for contact page recieved');
-  res.render('contact');
-});
+
 
 
 module.exports = router;
