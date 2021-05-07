cc745847088e66e7e5d7a7d8be4fffc8e3e052da
Parker Smith <parker@yeetdev.com>
2021-05-06 22:04:29 -0700

    Quizzes page

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,11 @@ router.get('/', (req, res) => {
   res.render('index');
 });
 
+router.get('/quizzes', (req, res) => {
+  console.log('Request for quizzes recieved');
+  res.render('quizzes');
+});
+
 router.get('/:quizCode', (req, res) => {
   console.log('Request for quiz page recieved: ' + req.params.quizCode);
   res.render('quiz', {quizCode: req.params.quizCode});
